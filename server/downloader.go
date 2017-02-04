764d44d10ade59a0f163435ab3faa17fa41359eb
Stephen Melinyshyn <stephen.melinyshyn@gmail.com>
2017-02-04 18:35:29 -0500

    Working backend

--- server/downloader.go
+++ server/downloader.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"hash/fnv"
+	"crypto/sha256"
+	"encoding/hex"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -9,6 +11,7 @@ import (
 
 // Download gets the file and saves it
 func Download(link string, ch chan string) {
+	log.Println("Got request to download", link)
 	resp, err := http.Get(link)
 	if err != nil {
 		log.Println("ERROR: fetching link", link, err)
@@ -16,17 +19,19 @@ func Download(link string, ch chan string) {
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-	filename = hash(link)
-	err = iotutil.WriteFile(filename, body, 777)
+	log.Println("Got body for link", link)
+	filename := hash(link)
+	err = ioutil.WriteFile(filename, body, 0777)
 	if err != nil {
-		log.Println("ERROR: enable to save file", string(body), err)
+		log.Println("ERROR: enable to save file", link, len(body), err)
 		ch <- "ERROR"
 	}
+	log.Println("Got file saved", filename)
 	ch <- filename
 }
 
-func hash(s string) uint32 {
-	h := fnv.New32a()
-	h.Write([]byte(s))
-	return h.Sum32()
+func hash(s string) string {
+	h := sha256.New()
+	io.WriteString(h, s)
+	return hex.EncodeToString(h.Sum(nil))
 }
