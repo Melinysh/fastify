877c9a74a8ca841c1a134690a6e02f68e42e213d
Stephen Melinyshyn <stephen.melinyshyn@gmail.com>
2017-02-05 10:14:23 -0500

    Latest changes

--- server/main.go
+++ server/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
 )
 
@@ -21,7 +26,31 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Panicln("reading response body", err)
 	}
 	log.Println(string(data))
+	b := bytes.NewReader(data)
+	go func() {
+		_, err = http.Post("http://ec2-54-84-75-4.compute-1.amazonaws.com:8080", "text/octet-stream", b)
+		if err != nil {
+			log.Println("error connecting to other server: (2)", err)
+		}
+	}()
+	b2 := bytes.NewReader(data)
+	go func() {
+		_, err = http.Post("http://ec2-52-206-125-141.compute-1.amazonaws.com:8080", "text/octet-stream", b2)
+		if err != nil {
+			log.Println("error connecting to other server (3) :", err)
+		}
+	}()
 
+	if _, err := os.Stat(Hash(string(data)) + ".torrent"); !os.IsNotExist(err) {
+		fmt.Fprintf(w, "http://ec2-54-83-190-222.compute-1.amazonaws.com:81/"+Hash(string(data))+".torrent")
+		log.Println("Returning cached value")
+		err = exec.Command("/bin/bash", "-c", "/home/ubuntu/c-t.sh "+Hash(string(data))).Start()
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		return
+	}
 	c := make(chan string)
 	defer close(c)
 	go Download(string(data), c)
@@ -38,4 +67,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
+}
+
+// Hash needed to be commenteded :(
+func Hash(s string) string {
+	h := sha256.New()
+	io.WriteString(h, s)
+	return hex.EncodeToString(h.Sum(nil))
 }
