2c6dcdc852afdbcc5589761e8ff397e84fc3809d
Stephen Melinyshyn <stephen.melinyshyn@gmail.com>
2017-02-04 19:08:27 -0500

    Trying something else

--- server/main.go
+++ server/main.go
@@ -5,7 +5,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os/exec"
 )
 
 func main() {
@@ -21,21 +20,30 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Panicln("reading response body", err)
 	}
 	log.Println(string(data))
-	c := make(chan string)
-	defer close(c)
-	go Download(string(data), c)
-	for {
-		select {
-		case filename, _ := <-c:
-			out, err := exec.Command("/bin/bash", "-c", "/home/ubuntu/c-t.sh "+filename).Output()
-			if err != nil {
-				log.Panicln(err)
-			}
 
-			fmt.Fprintf(w, "http://ec2-54-83-190-222.compute-1.amazonaws.com:81/"+filename+".torrent")
-			log.Println("Returned link to torrent for", filename)
-			return
-
-		}
+	filedata, err := ioutil.ReadFile(string(data))
+	if err != nil {
+		log.Println(err)
+		fmt.Fprintln(w, err.Error())
+		return
 	}
+	fmt.Fprintln(w, string(filedata))
+
+	/*	c := make(chan string)
+		defer close(c)
+		go Download(string(data), c)
+		for {
+			select {
+			case filename, _ := <-c:
+				err := exec.Command("/bin/bash", "-c", "/home/ubuntu/c-t.sh "+filename).Start()
+				if err != nil {
+					log.Panicln(err)
+				}
+
+				fmt.Fprintf(w, "http://ec2-54-83-190-222.compute-1.amazonaws.com:81/"+filename+".torrent")
+				log.Println("Returned link to torrent for", filename)
+				return
+
+			}
+		}*/
 }
